5be8c409c8063f8db999e64f707414689b1ca2f2
kyun8852 <kyun8852@gmail.com>
2022-02-17 16:55:01 +0900

    sad

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+const app = express();
+
+app.listen(5020);
+
+console.log("5020번 에서 작동중");
+
+app.get("/", (req, res) => {
+  function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  var words = ["위험", "경고", "주의", "정상"];
+  var word = words[Math.floor(Math.random() * words.length)];
+  var eng = ["Very high", "High", "Medium", "Low", "Very Low"];
+  var engDanger = eng[Math.floor(Math.random() * eng.length)];
+  res.send([
+    {
+      "1부터100": randomNumber(1, 100).toFixed(1),
+      "1부터10": randomNumber(1, 10).toFixed(1),
+      "1부터20": randomNumber(1, 20).toFixed(1),
+      "1부터40": randomNumber(1, 40).toFixed(1),
+      "1부터20": randomNumber(1, 20).toFixed(1),
+      "1부터30": randomNumber(1, 30).toFixed(1),
+    },
+    {
+      "km/hr": randomNumber(1, 100).toFixed(1),
+      dbm: randomNumber(1, 20).toFixed(1),
+      Knot: randomNumber(1, 40).toFixed(1),
+      cm: randomNumber(1, 20).toFixed(1),
+      warning: word,
+      warning_2: engDanger,
+    },
+  ]);
+});
