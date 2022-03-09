21c46877dffdbbca161f6952e4e7b0f0a894152d
kyun8852 <kyun8852@gmail.com>
2022-03-09 14:47:44 +0900

    mai

--- index.js
+++ index.js
@@ -9,32 +9,37 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.listen(process.env.PORT || 5020);
 
-console.log("5020번 에서 작동중");
-
 app.get("/", (req, res) => {
   function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
   }
-  var words = ["위험", "경고", "주의", "정상"];
-  var word = words[Math.floor(Math.random() * words.length)];
-  var eng = ["Very high", "High", "Medium", "Low", "Very Low"];
-  var engDanger = eng[Math.floor(Math.random() * eng.length)];
+  let date_ob = new Date();
+
+  let date = ("0" + date_ob.getDate()).slice(-2);
+
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+  let year = date_ob.getFullYear();
+
+  let hours = date_ob.getHours();
+
+  let minutes = date_ob.getMinutes();
+
+  let seconds = date_ob.getSeconds();
+
   res.send([
     {
-      "1부터100": randomNumber(1, 100).toFixed(1),
-      "1부터10": randomNumber(1, 10).toFixed(1),
-      "1부터20": randomNumber(1, 20).toFixed(1),
-      "1부터40": randomNumber(1, 40).toFixed(1),
-      "1부터20": randomNumber(1, 20).toFixed(1),
-      "1부터30": randomNumber(1, 30).toFixed(1),
-    },
-    {
-      "km/hr": randomNumber(1, 100).toFixed(1),
-      dbm: randomNumber(1, 20).toFixed(1),
-      Knot: randomNumber(1, 40).toFixed(1),
-      cm: randomNumber(1, 20).toFixed(1),
-      warning: word,
-      warning_2: engDanger,
+      Date: year + "-" + month + "-" + date,
+      Time: hours + ":" + minutes + ":" + seconds,
+      ID1_Load_VOL: randomNumber(400, 450).toFixed(0),
+      ID1_MCU_Temp: randomNumber(22, 23).toFixed(1),
+      ID1_RSSI: randomNumber(45, 50).toFixed(0),
+      ID2_Load_VOL: randomNumber(550, 600).toFixed(0),
+      ID2_MCU_Temp: randomNumber(24, 25).toFixed(1),
+      ID3_Load_VOL: randomNumber(500, 520).toFixed(0),
+      ID3_MCU_Temp: randomNumber(23, 25).toFixed(1),
+      weather_hazard: randomNumber(1, 2).toFixed(0),
+      Risk_of_collision: randomNumber(8, 10).toFixed(0),
     },
   ]);
 });
