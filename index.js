a254a6e8bf5f56859c2e8128ebbe409496cd2b95
kyun8852 <kyun8852@gmail.com>
2022-03-09 15:41:51 +0900

    mai

--- index.js
+++ index.js
@@ -34,10 +34,15 @@ app.get("/", (req, res) => {
       ID1_Load_VOL: randomNumber(400, 450).toFixed(0),
       ID1_MCU_Temp: randomNumber(22, 23).toFixed(1),
       ID1_RSSI: randomNumber(45, 50).toFixed(0),
+
       ID2_Load_VOL: randomNumber(550, 600).toFixed(0),
       ID2_MCU_Temp: randomNumber(24, 25).toFixed(1),
+      ID2_RSSI: randomNumber(50, 60).toFixed(0),
+
       ID3_Load_VOL: randomNumber(500, 520).toFixed(0),
       ID3_MCU_Temp: randomNumber(23, 25).toFixed(1),
+      ID3_RSSI: randomNumber(45, 60).toFixed(0),
+
       weather_hazard: randomNumber(1, 2).toFixed(0),
       Risk_of_collision: randomNumber(8, 10).toFixed(0),
     },
